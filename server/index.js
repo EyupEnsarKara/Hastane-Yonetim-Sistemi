7b24744303fd0b2baf20a1f8604d9d68e5fa5304
Yunus Hanifi <oyunushanifi@gmail.com>
2024-05-06 18:23:52 +0300

    feat: Add endpoint to add a patient to the database

--- server/index.js
+++ server/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const { host, database, password, user, port } = require('./config.json')
+const mysql = require('mysql2');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const connection = mysql.createConnection({
+    host: host,
+    database: database,
+    password: password,
+    user: user
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Veritabanına bağlanılamadı.");
+        return;
+    }
+    console.log("Veritabanına bağlanıldı.");
+})
 
 
-
-
+app.post('/addPatient', (req, res) => {
+    const { name, surName, birthDate, gender, phoneNumber, adress } = req.body;
+    connection.query(`
+    INSERT INTO Hastalar (Ad, Soyad, DogumTarihi, Cinsiyet, TelefonNumarasi, Adres)
+             VALUES (?, ?, ?, ?, ?, ?)
+    `, [name, surName, birthDate, gender, phoneNumber, adress], (err) => {
+        if (err)
+            console.log("Veri eklemede hata oluştu");
+    })
+})
 
 
 
