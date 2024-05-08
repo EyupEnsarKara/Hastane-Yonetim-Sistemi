49bfdeff3869ea9eecdf4e01a97e673464a8fa67
EyupEnsar <118618895+EyupEnsarKara@users.noreply.github.com>
2024-05-09 02:26:36 +0300

    chore: Update LoginContainer to use index instead of string for active state

--- react/src/Components/LoginContainer.jsx
+++ react/src/Components/LoginContainer.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
-import '../css/LoginContainer.css'
-
+import React, { useState } from 'react';
+import '../css/LoginContainer.css';
 
 function LoginContainer() {
-
-    const [active, setActive] = useState('Patient');
+    const [active, setActive] = useState(0);
     const [phoneNumber, setPhoneNumber] = useState('');
     const [password, setPassword] = useState('');
-    const updateActive = (type) => {
-        setActive(type);
+
+    const updateActive = (index) => {
+        setActive(index);
     };
 
     const loginCheck = () => {
@@ -17,22 +16,19 @@ function LoginContainer() {
         console.log("Password:", password);
     };
 
-
-
     return (
         <div className='loginDiv'>
             <div className='button-container'>
-                <button className={`login-type-selector ${active === 'Patient' ? 'active' : ''}`} onClick={() => updateActive('Patient')}>Patient</button>
-                <button className={`login-type-selector ${active === 'Doctor' ? 'active' : ''}`} onClick={() => updateActive('Doctor')}>Doctor</button>
-                <button className={`login-type-selector ${active === 'Admin' ? 'active' : ''}`} onClick={() => updateActive('Admin')}>Admin</button>
-                <div className="slider"></div>
+                <button className={`login-type-selector ${active === 0 ? 'active' : ''}`} onClick={() => updateActive(0)}>Patient</button>
+                <button className={`login-type-selector ${active === 1 ? 'active' : ''}`} onClick={() => updateActive(1)}>Doctor</button>
+                <button className={`login-type-selector ${active === 2 ? 'active' : ''}`} onClick={() => updateActive(2)}>Admin</button>
+                <div className="slider" style={{ transform: `translateX(${active * 100}%)` }}></div>
             </div>
             <input className='login-input' type="text" placeholder='Phone Number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
             <input className='login-input' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
             <button className='login-button' onClick={loginCheck}>Click</button>
         </div>
-
-    )
+    );
 }
 
-export default LoginContainer
+export default LoginContainer;
