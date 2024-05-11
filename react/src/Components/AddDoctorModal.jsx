6fa367ec1e64d5d67c7ce28a8b30b46369c89541
Yunus Hanifi <oyunushanifi@gmail.com>
2024-05-11 23:59:56 +0300

    feat: Add AdminDoctors component to the Admin page

---
+++ react/src/Components/AddDoctorModal.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import '../css/AddPatientModal.css'
+import axios from 'axios'
+import { host, port } from '../../config.json'
+function AddDoctorModal({ modalfunc }) {
+    const [name, setName] = useState('');
+    const [surName, setSurName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [birthDate, setBirthDate] = useState('');
+    const [gender, setGender] = useState('');
+    const [address, setAddress] = useState('');
+
+
+    const addPatient = () => {
+        axios.post(`https://${host}:${port}/addPatient`, {
+            name: name,
+            birthDate: birthDate,
+            surName: surName,
+            password: password,
+            gender: gender,
+            phoneNumber: phoneNumber,
+            address: address
+        }).then(res => {
+            if (res.data.status == "ok") {
+                alert("kullanıcı ekleme başarılı");
+                modalfunc();
+            }
+        })
+    };
+
+
+    return (
+        <div className="modal">
+            <div className="overlay" onClick={() => (modalfunc())}></div>
+            <div className="modal-content">
+                <button className="close-modal">kap</button>
+                <h2>Add Doctor</h2>
+                <input className='login-input' type="text" placeholder='Name' onChange={(e) => (setName(e.target.value))} />
+                <input className='login-input' type="text" placeholder='SurName' onChange={(e) => (setSurName(e.target.value))} />
+                <input className='login-input' type="password" placeholder='Password' onChange={(e) => (setPassword(e.target.value))} />
+                <input className='login-input' type="date" placeholder='Birthdate' onChange={(e) => (setBirthDate(e.target.value))} />
+                <select className='login-input' onChange={(value) => (setGender(value.target.value))}>
+                    <option value="">Select Gender</option>
+                    <option value="woman">Woman</option>
+                    <option value="man">Man</option>
+                </select>
+                <input className='login-input' type="tel" placeholder='Phone Number' onChange={(e) => (setPhoneNumber(e.target.value))} />
+                <input className='login-input' type="text" placeholder='Address' onChange={(e) => (setAddress(e.target.value))} />
+
+                <button className='login-button' onClick={addPatient}>Click</button>
+            </div>
+        </div>
+    )
+}
+
+export default AddDoctorModal
---
+++ react/src/Pages/Admin/AdminDoctors.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import Dashboard from '../../Components/Dashboard'
+import AddPatientModal from '../../Components/AddPatientModal';
+import AddDoctorModal from '../../Components/AddDoctorModal';
+
+//icon importları
+import { MdDelete } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
+
+function AdminDoctors() {
+    const doktorListesi = [
+        { id: 1, ad: "Ahmet", soyad: "Yılmaz", dogumTarihi: "1990-05-15", cinsiyet: "Erkek", uzmanlikAlani: "Hipertansiyon" },
+        { id: 2, ad: "Ayşe", soyad: "Kara", dogumTarihi: "1985-10-20", cinsiyet: "Kadın", uzmanlikAlani: "Diyabet" },
+        { id: 10, ad: "Mehmet", soyad: "Demir", dogumTarihi: "1992-08-25", cinsiyet: "Erkek", uzmanlikAlani: "Kardiyoloji" },
+        { id: 3, ad: "Ali", soyad: "Can", dogumTarihi: "1995-03-12", cinsiyet: "Erkek", uzmanlikAlani: "Ortopedi" },
+        { id: 4, ad: "Ayşe", soyad: "Yıldız", dogumTarihi: "1988-07-18", cinsiyet: "Kadın", uzmanlikAlani: "Göz Hastalıkları" },
+        { id: 5, ad: "Mehmet", soyad: "Kaya", dogumTarihi: "1993-11-05", cinsiyet: "Erkek", uzmanlikAlani: "Kulak Burun Boğaz" },
+        { id: 6, ad: "Fatma", soyad: "Şahin", dogumTarihi: "1991-09-30", cinsiyet: "Kadın", uzmanlikAlani: "Nöroloji" },
+        { id: 7, ad: "Ahmet", soyad: "Demir", dogumTarihi: "1987-04-25", cinsiyet: "Erkek", uzmanlikAlani: "Kardiyoloji" },
+        { id: 8, ad: "Zeynep", soyad: "Yılmaz", dogumTarihi: "1994-02-15", cinsiyet: "Kadın", uzmanlikAlani: "Dermatoloji" },
+        { id: 9, ad: "Mustafa", soyad: "Aydın", dogumTarihi: "1990-06-20", cinsiyet: "Erkek", uzmanlikAlani: "Psikiyatri" },
+        { id: 10, ad: "Elif", soyad: "Kara", dogumTarihi: "1989-08-10", cinsiyet: "Kadın", uzmanlikAlani: "Kadın Hastalıkları" },
+        { id: 11, ad: "Ali", soyad: "Yılmaz", dogumTarihi: "1992-12-05", cinsiyet: "Erkek", uzmanlikAlani: "Ortopedi" },
+        { id: 12, ad: "Ayşe", soyad: "Kara", dogumTarihi: "1986-07-30", cinsiyet: "Kadın", uzmanlikAlani: "Göz Hastalıkları" }
+
+    ];
+    const [ad, setAd] = useState('');
+    const [soyad, setSoyad] = useState('');
+    const [dogumTarihi, setDogumTarihi] = useState('');
+    const [cinsiyet, setCinsiyet] = useState('');
+    const [hastalikGecmisi, setHastalikGecmisi] = useState('');
+    const [modalState, setModalState] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
+
+    const lastIndex = currentPage * itemsPerPage;
+    const firstIndex = lastIndex - itemsPerPage;
+    const currentItems = doktorListesi.slice(firstIndex, lastIndex);
+
+    const totalPages = Math.ceil(doktorListesi.length / itemsPerPage);
+
+    const handleClick = (event) => {
+        setCurrentPage(Number(event.target.id));
+    };
+    const toggleModalState = () => {
+        setModalState(!modalState);
+    }
+
+
+    return (
+        <Dashboard>
+
+
+            <div className="container">
+                <h2>Doctors</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Ad</th>
+                            <th>Soyad</th>
+                            <th>Doğum Tarihi</th>
+                            <th>Cinsiyet</th>
+                            <th>Uzmanlık alanı</th>
+                            <th>İşlemler</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentItems.map(hasta => (
+                            <tr key={hasta.id}>
+                                <td>{hasta.ad}</td>
+                                <td>{hasta.soyad}</td>
+                                <td>{hasta.dogumTarihi}</td>
+                                <td>{hasta.cinsiyet}</td>
+                                <td>{hasta.uzmanlikAlani}</td>
+                                <td>
+                                    <CiEdit className='icon' />
+                                    <MdDelete className='icon' />
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                </table>
+                <div>
+                    {Array(totalPages).fill().map((_, index) => (
+                        <button key={index + 1} id={index + 1} onClick={handleClick}>
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
+                <button onClick={() => (setModalState(!modalState))}>Add Doctor</button>
+                {modalState && (<AddDoctorModal modalfunc={toggleModalState} />)}
+
+            </div>
+        </Dashboard>
+    );
+}
+
+export default AdminDoctors
