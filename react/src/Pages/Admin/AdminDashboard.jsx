cd0014121a28464037bbe07d4577ee4803ef8647
Yunus Hanifi <oyunushanifi@gmail.com>
2024-05-15 16:27:25 +0300

    feat: Add file upload functionality to AdminDashboard

--- react/src/Pages/Admin/AdminDashboard.jsx
+++ react/src/Pages/Admin/AdminDashboard.jsx
@@ -1,9 +1,53 @@
 import React, { useState } from 'react'
 import '../../css/AdminDashboard.css'
 import Dashboard from '../../Components/Dashboard'
+import axios from 'axios';
 
 function AdminDashboard() {
     const [count, setCount] = useState(0);
+
+    const [file, setFile] = useState(null);
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    }
+
+    const handleUpload = () => {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("folderId", "30abaf64-0d13-4d98-999c-60e5517dc1db");
+
+        axios.post('https://store10.gofile.io/contents/uploadfile', formData, {
+            headers: {
+                'Authorization': 'Bearer KOHEmxwonfmF3LtUJigY9aiePLis53jw',
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+    const handleCheck = () => {
+
+
+        axios.get('https://api.gofile.io/contents/ce93cb51-f4f1-4bcf-a810-01139ecf9bb1', {
+            headers: {
+                'Authorization': 'Bearer KOHEmxwonfmF3LtUJigY9aiePLis53jw'
+            }
+        })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+
+
+
     return (
         <Dashboard>
             <div className='dashboard-total-patients'>
@@ -13,6 +57,10 @@ function AdminDashboard() {
             <button onClick={() => {
                 confirm("onayla beni");
             }}>asds</button>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Yükle</button>
+            <button onClick={handleCheck}>test</button>
+            {/* <img src="https://store5.gofile.io/download/web/ce93cb51-f4f1-4bcf-a810-01139ecf9bb1/thumb_image.png" alt="ssss" /> */}
         </Dashboard>
     )
 }
